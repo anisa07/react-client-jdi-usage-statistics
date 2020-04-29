e819c2990d8ffe0015eb7e080bf3d4d805aa3f2d
anisa07 <askany07@gmail.com>
2020-04-29 23:59:47 +0300

    add statistics

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,19 +4,22 @@ import Item from '../../components/Item/Item';
 import { getInfo } from '../../helpers/info';
 import './style.scss';
 
-const Home = inject('store')(observer((props) =>{
+const Home = inject('store')(observer((props) => {
 	const { setMessage, setInfo, info, setAuth } = props.store;
+	const [result, setResult] = useState(undefined);
 
 	useEffect(() => {
 		try {
-			const fetchDataAsync = async () => {
-				const data  = await getInfo(setMessage);
+			const fetchDataAsync = async() => {
+				const data = await getInfo(setMessage);
 				if (!data) {
 					setAuth('', false);
 					props.history.push('/register')
 				} else {
-					console.log(data);
 					setInfo(data);
+					if (data.results && !!data.results.length) {
+						setResult(data.results[data.results.length - 1])
+					}
 				}
 			};
 			fetchDataAsync();
@@ -25,15 +28,20 @@ const Home = inject('store')(observer((props) =>{
 		}
 	}, []);
 
-	return(
-		<div className="container">
-			<div className="items">
-				{/*<Item label="Test" value={3}/>*/}
-				{/*<Item label="Test3" value={6}/>*/}
-				{/*<Item label="Test hjkhjk" value={3}/>*/}
-				{/*<Item label="Test3" value={6}/>*/}
-			</div>
-		</div>
-)}));
+	return (<>
+			{result && <div className="container">
+				<div className="items">
+					<Item label="Unique users" value={result.uniqueUsers}/>
+					<Item label="New users last month" value={result.newUsers}/>
+					<Item label="Sessions last month" value={result.sessions}/>
+					<Item label="Active users last month" value={result.activeUsers}/>
+					<Item label="Actively used versions" value={(info.activeUsedVersions || []).join(', ')}/>
+					<Item label="Last month usage intensity" value={info.lastMonthUsageIntensity}/>
+					<Item label="New projects last month" value={result.newProjects}/>
+					<Item label="Active projects last month" value={result.activeProjects}/>
+				</div>
+			</div>}
+		</>)
+}));
 
 export default Home;
