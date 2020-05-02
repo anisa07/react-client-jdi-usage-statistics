17b00b32bb2dcdb40d1e6b6cfc13fae30d460e18
anisa07 <askany07@gmail.com>
2020-05-02 13:35:28 +0300

    add visualization

---
+++ src/components/Charts/Chart.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {XYPlot, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, LineSeries, MarkSeries} from 'react-vis';
+import './style.scss';
+
+class Chart extends React.Component {
+	formatDate = (date) => {
+		const d = new Date(date);
+		const m = d.getMonth()+1 > 9 ? d.getMonth()+1 : `0${d.getMonth()+1}`;
+		return `${d.getDate()}/${m}`;
+	};
+
+	render() {
+		return (
+			<div className="chart">
+				<XYPlot
+					width={600}
+					height={300}
+				>
+					<VerticalGridLines/>
+					<HorizontalGridLines/>
+					<XAxis
+						title={this.props.x || "days"}
+						attr="x"
+						attrAxis="y"
+						orientation="bottom"
+						tickFormat={(d) => {return this.formatDate(d)}}
+					/>
+					<YAxis
+						title={ this.props.y }
+						attr="y"
+						attrAxis="x"
+						orientation="left"
+					/>
+					<LineSeries
+						curve={'curveMonotoneX'}
+						data={this.props.data}/>
+					{this.props.data2 && <LineSeries
+						stroke="fuchsia"
+						curve={'curveMonotoneX'}
+						data={this.props.data2}/> }
+				</XYPlot>
+			</div>
+		);
+	}
+};
+
+export default Chart;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { observer, inject } from "mobx-react";
 import Item from '../../components/Item/Item';
+import Chart from '../../components/Charts/Chart';
 import { getInfo } from '../../helpers/info';
 import './style.scss';
 
 const Home = inject('store')(observer((props) => {
 	const { setMessage, setInfo, info, setAuth } = props.store;
-	const [result, setResult] = useState(undefined);
+	const lastMonth = Date.now() - (3600000 * 24 * 30);
+	const [ result, setResult ] = useState(undefined);
+	const [ totalUsers, setTotalUsers ] = useState([]);
+	// const [totalProjects, setTotalProjects] = useState([]);
+	const [ intensity, setIntensity ] = useState([]);
+	const [ activeSessions, setActiveSessions ] = useState([]);
+	const [ newProjects, setNewProjects ] = useState([]);
+	const [ activeProjects, setActiveProjects ] = useState([]);
+	const [ activeUsers, setActiveUsers ] = useState([]);
+	const [ newUsers, setNewUsers ] = useState([]);
 
 	useEffect(() => {
 		try {
@@ -14,11 +24,48 @@ const Home = inject('store')(observer((props) => {
 				const data = await getInfo(setMessage);
 				if (!data) {
 					setAuth('', false);
-					props.history.push('/register')
+					props.history.push('/register');
 				} else {
 					setInfo(data);
 					if (data.results && !!data.results.length) {
-						setResult(data.results[data.results.length - 1])
+						setResult(data.results[data.results.length - 1]);
+					}
+
+					const tUsers = [];
+					const tProjects = [];
+					const aSessions = [];
+					const nUsers = [];
+					const aUsers = [];
+					const nProjects = [];
+					const acProjects = [];
+
+					data.results.forEach(dayData => {
+						const d = new Date(dayData.resultDate);
+						if (d.getTime() > lastMonth) {
+							tUsers.push({ x: d, y: dayData.uniqueUsers });
+							aSessions.push({ x: d, y: dayData.sessions});
+							nUsers.push({ x: d, y: dayData.newUsers });
+							aUsers.push({ x: d, y: dayData.activeUsers });
+							nProjects.push({ x: d, y: dayData.newProjects});
+							acProjects.push({ x: d, y: dayData.activeProjects})
+						}
+					});
+					setTotalUsers(tUsers);
+					setActiveSessions(aSessions);
+					setNewProjects(nProjects);
+					setActiveProjects(acProjects);
+					setActiveUsers(aUsers);
+					setNewUsers(nUsers);
+
+					if (data.intensity && !!data.intensity.length) {
+						const uIntensity = [];
+						data.intensity.forEach(intensObj => {
+							const d = new Date(intensObj.intensityDate);
+							if (d.getTime() > lastMonth) {
+								uIntensity.push({ x: d, y: intensObj.intensity });
+							}
+						});
+						setIntensity(uIntensity);
 					}
 				}
 			};
@@ -29,7 +76,8 @@ const Home = inject('store')(observer((props) => {
 	}, []);
 
 	return (<>
-			{result && <div className="container">
+		{result && <>
+			<div className="container">
 				<div className="items">
 					<Item label="Unique users" value={result.uniqueUsers}/>
 					<Item label="New users last month" value={result.newUsers}/>
@@ -40,8 +88,16 @@ const Home = inject('store')(observer((props) => {
 					<Item label="New projects last month" value={result.newProjects}/>
 					<Item label="Active projects last month" value={result.activeProjects}/>
 				</div>
-			</div>}
-		</>)
+				<div className="charts">
+					<Chart data={totalUsers} y="unique users"/>
+					<Chart data={activeSessions} y="active sessions"/>
+					<Chart data={intensity} y="intensity"/>
+					<Chart data={newProjects} data2={activeProjects} y="new, active projects"/>
+					<Chart data={newUsers} data2={activeUsers} y="new, active users"/>
+				</div>
+			</div>
+		</>}
+	</>)
 }));
 
 export default Home;
